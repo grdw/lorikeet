5526d9053def36d1369a0a6bfb61f1144ff1504a
Gerard Westerhof <gerard@grdw.nl>
2016-01-28 22:32:31 +0100

    Adding index.haml + style.sass + misc

--- deploy/deploy_commands.sh
+++ deploy/deploy_commands.sh
@@ -1,16 +1,1 @@
-echo "> Starting deploy"
-cd /www/
-rm -rf drunkenmarias
-echo "> Removed old project"
-git clone https://github.com/grdw/drunkenmarias.git
-
-echo "> Removing deploy files"
-rm -rf drunkenmarias/deploy/
-rm drunkenmarias/README.md
-
-echo "> Moving html files to root"
-cp -R drunkenmarias/ .
-rm -rf drunkenmarias
-
 bash './cache-purge'
-echo "> Finished"
